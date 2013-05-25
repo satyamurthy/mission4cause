af40ad890551d6c8f635dfd07a6b3e4637978ca0
Satya Murthy <satya@satya.me>
2013-05-24 23:30:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mission4cause::Application.config.secret_token = 'a87a991e28a8446d7f6d45b642d316e71d99052a58e8a79ccac587048bf329afe8f7b3a1321fb0df69885c65fb49f880e929d4f61d868bcef4d6204808dbaa9b'
